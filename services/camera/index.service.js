16e922faf0ffba4263eb02dc1b8b64fe4b7adbc9
Tristram Woolf <tristram@woolfsystems.com>
2020-10-06 18:17:50 +0200

    capture extra code for investigation

--- services/camera/index.service.js
+++ services/camera/index.service.js
@@ -1,11 +1,30 @@
-const {FSWebcam} = require('node-webcam')
+const {FSWebcam, capture} = require('node-webcam')
+const fs = require('fs')
 
-var cam = new FSWebcam({
-    device: '/dev/video0',
-    output: 'png'
+
+fs.readdir('/sys/class/video4linux/', (err, files) => {
+    if(err){
+        return console.error('Unable to scan for v4l2 capture devices: ' + err)
+    }
+    let camera_devices = []
+    files.forEach((deviceName) => {
+        camera_devices.push(deviceName)
+    })
+    console.log(camera_devices)
 })
-console.log('cam',cam)
-cam.capture( "cap01", function( err, data ) {
-    if(!err)
-        console.log("captured")
-})
+
+function cam_capture(device_id){
+    var cam = new FSWebcam({
+        device: '/dev/video0',
+        output: 'png'
+    })
+    console.log('cam',cam)
+    cam.capture( "cap01", function( err, data ) {
+        if(!err)
+            console.log("captured")
+    })
+}
+
+var cam = require('linuxcam')
+cam.start("/dev/video0", 1200, 798)
+var frame = cam.frame()
