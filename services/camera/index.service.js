f19c92013be0e6c7aa087fa6bb5a78ff9af044f6
Tristram Woolf <tristram@woolfsystems.com>
2020-10-12 17:21:07 +0200

    camera calibration routines

--- services/camera/index.service.js
+++ services/camera/index.service.js
@@ -1,6 +1,21 @@
 const {FSWebcam, capture} = require('node-webcam')
+const {ARToolkit,ARController} = require('artoolkit5-js')
+const path = require('path')
 const fs = require('fs')
+const Image = require('htmlimage')
+
 var _capture_index = 1
+
+var i = new Image()
+i.onload = function() {
+  // do something with image.imageData.data (which is a pixel buffer)
+ console.log('LOADED')
+ ARController.initWithDimensions(1280,768, '/data/camera_para.dat')
+}
+i.onerror = function(err) {
+  // OHNOEZ!
+}
+i.src = `${__dirname}/cap01.png`
 
 var cam = new FSWebcam({
     device: '/dev/video0',
@@ -14,6 +29,42 @@ module.exports = {
     name: 'camera',
     middlewares: [],
     actions: {
+        orientation: {
+            params: {
+
+            },
+            handler(ctx){
+                return new this.Promise((resolve,reject)=>{
+                    var param = new ARCameraParam();
+
+                    param.onload = function () {
+                      var img = document.getElementById('my-image');
+                      var ar = new ARController(img.width, img.height, param);
+                  
+                      // Set pattern detection mode to detect both pattern markers and barcode markers.
+                      // This is more error-prone than detecting only pattern markers (default) or only barcode markers.
+                      //
+                      // For barcode markers, use artoolkit.AR_MATRIX_CODE_DETECTION
+                      // For pattern markers, use artoolkit.AR_TEMPLATE_MATCHING_COLOR
+                      //
+                      ar.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_COLOR_AND_MATRIX);
+                  
+                      ar.addEventListener('markerNum', function (ev) {
+                        console.log('got markers', markerNum);
+                      });
+                      ar.addEventListener('getMarker', function (ev) {
+                        console.log('found marker?', ev);
+                      });
+                      ar.loadMarker('Data/patt.hiro', function (marker) {
+                        console.log('loaded marker', marker);
+                        ar.process(img);
+                      });
+                  };
+                  
+                    param.src = 'Data/camera_para.dat';
+                })
+            }
+        },
         list: {
             params: {
 
@@ -22,7 +73,7 @@ module.exports = {
                 return new this.Promise((resolve,reject)=>{
                     fs.readdir('/sys/class/video4linux/', (err, files) => {
                         if(err){
-                            return console.error('Unable to scan for v4l2 capture devices: ' + err)
+                            return reject('Unable to scan for v4l2 capture devices: ' + err)
                         }
                         let camera_devices = []
                         files.forEach((deviceName) => {
@@ -42,7 +93,7 @@ module.exports = {
                 return new this.Promise((resolve,reject)=>{
                     cam.capture( `cap${String(_capture_index).padStart(4,'0')}`, function( err, data ) {
                         if(err)
-                            return reject(err)
+                            return reject(err   )
                         _capture_index++
                         resolve()
                     })
